8cc4fa63ab7a19c2aa0c155122b4dee4b7edddd1
bwfurness <bwfurness@gmail.com>
2019-05-15 11:23:24 -0400

    Added last names

--- src/studentarrayexample/StudentTester.java
+++ src/studentarrayexample/StudentTester.java
@@ -16,15 +16,15 @@ public class StudentTester {
 	public static void main(String[] args) {
 		// pre-populate the student array with our 5 static students.
 		Student[] students = {
-					new Student("Brian", 28, 193),
-					new Student("Rick", 20, 40142),
-					new Student("Mary", 18, 13450),
-					new Student("James", 23, 3145),
+					new Student("Brian Furness", 28, 193),
+					new Student("Rick Morty", 20, 40142),
+					new Student("Mary Jane", 18, 13450),
+					new Student("James James", 23, 3145),
 					new Student()
 				};
 		
 		// fill in the last student.
-		students[4].setName("Sarah");
+		students[4].setName("Sarah Oslin");
 		students[4].setAge(27);
 		students[4].setStudentId(13425);
 		
